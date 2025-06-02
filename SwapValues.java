b48944a4a47aac420c3f81029717b879bb558da1
Khubaib Husain <khubaibhusain45@gmail.com>
2025-06-03 00:24:19 +0500

    Create SwapValues.java

---
+++ SwapValues.java
@@ -0,0 +1,16 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c;
+        System.out.println("Value of a is " + a + " and b is " + b);
+        System.out.println("Swapping Completed.........");
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("Value of a is " + a + " and b is " + b);
+
+    }
+}
